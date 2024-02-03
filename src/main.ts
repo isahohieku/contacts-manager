651d09c79d58ea1930af77c8dd2327535ad2718f
Isah Ohieku <isahohieku@gmail.com>
2024-02-03 16:25:53 +0100

    Add global helpers and configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SerializerInterceptor } from './utils/serializer.interceptor';
+import validationOptions from './utils/validation-options';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  app.enableShutdownHooks();
+  app.setGlobalPrefix(configService.get('app.apiPrefix'), {
+    exclude: ['/'],
+  });
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalInterceptors(new SerializerInterceptor());
+  app.useGlobalPipes(new ValidationPipe(validationOptions));
+
   await app.listen(3000);
 }
 bootstrap();
