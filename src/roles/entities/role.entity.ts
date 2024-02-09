86e6139fc952b4d6f885840af2f91d53f568f77c
Isah Ohieku <isahohieku@gmail.com>
2024-02-09 19:26:27 +0100

    Add example to role and status entity

--- src/roles/entities/role.entity.ts
+++ src/roles/entities/role.entity.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { Allow } from 'class-validator';
 import { EntityHelper } from 'src/utils/entity-helper';
 
 @Entity()
 export class Role extends EntityHelper {
+  @ApiProperty({ example: 1 })
   @PrimaryColumn()
   id: number;
 
   @Allow()
+  @ApiProperty({ example: 'Admin' })
   @Column()
   name?: string;
 }
--- src/statuses/entities/status.entity.ts
+++ src/statuses/entities/status.entity.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { Allow } from 'class-validator';
 import { EntityHelper } from 'src/utils/entity-helper';
 
 @Entity()
 export class Status extends EntityHelper {
+  @ApiProperty({ example: 1 })
   @PrimaryColumn()
   id: number;
 
   @Allow()
+  @ApiProperty({ example: 'Active' })
   @Column()
   name?: string;
 }
