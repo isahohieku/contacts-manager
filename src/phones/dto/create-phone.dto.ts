6a7db6c2e7fd9091ae32bae34411c16f00de79fd
Isah Ohieku <isahohieku@gmail.com>
2024-02-10 21:30:57 +0100

    Add create phone dto

--- src/phones/dto/create-phone.dto.ts
+++ src/phones/dto/create-phone.dto.ts
@@ -1,1 +1,16 @@
-export class CreatePhoneDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, Validate } from 'class-validator';
+import { IsExist } from 'src/utils/validators/is-exists.validator';
+import { PhoneType } from 'src/phone-types/entities/phone-type.entity';
+
+export class CreatePhoneDto {
+  @ApiProperty({ example: 'Doe' })
+  @IsOptional()
+  phone_number?: string | null;
+
+  @ApiProperty({ type: PhoneType })
+  @Validate(IsExist, ['PhoneType', 'id'], {
+    message: 'phoneTypeNotExists',
+  })
+  phone_type?: PhoneType;
+}
