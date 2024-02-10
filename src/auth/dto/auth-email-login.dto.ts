ccd1cb0ad5dd9aee95abb46d194d0e6b921d1245
Isah Ohieku <isahohieku@gmail.com>
2024-02-10 06:55:31 +0100

    Refactor auth email login dto

--- src/auth/dto/auth-email-login.dto.ts
+++ src/auth/dto/auth-email-login.dto.ts
@@ -1,14 +1,11 @@
-import { IsNotEmpty, Validate } from 'class-validator';
-import { IsExist } from 'src/utils/validators/is-exists.validator';
+import { IsNotEmpty, IsEmail } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthEmailLoginDto {
   @ApiProperty({ example: 'john.doe@example.com' })
   @Transform(({ value }) => value.toLowerCase().trim())
-  @Validate(IsExist, ['User'], {
-    message: 'emailNotExists',
-  })
+  @IsEmail()
   email: string;
 
   @ApiProperty()
