3922c0002e73dd384cf17e9c26d6cfba2ec5a55c
Isah Ohieku <isahohieku@gmail.com>
2024-02-10 08:32:56 +0100

    Implement findAll service

--- src/contacts/contacts.controller.ts
+++ src/contacts/contacts.controller.ts
@@ -7,12 +7,14 @@ import {
   Param,
   Delete,
   UseGuards,
+  Request,
 } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { CreateContactDto } from './dto/create-contact.dto';
 import { UpdateContactDto } from './dto/update-contact.dto';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { User } from 'src/users/entity/user.entity';
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'))
@@ -25,18 +27,18 @@ export class ContactsController {
   constructor(private readonly contactsService: ContactsService) {}
 
   @Post()
-  create(@Body() createContactDto: CreateContactDto) {
-    return this.contactsService.create(createContactDto);
+  create(@Request() request, @Body() createContactDto: CreateContactDto) {
+    return this.contactsService.create(request.user as User, createContactDto);
   }
 
   @Get()
-  findAll() {
-    return this.contactsService.findAll();
+  findAll(@Request() request) {
+    return this.contactsService.findAll(request.user as User);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.contactsService.findOne(+id);
+  findOne(@Request() request, @Param('id') id: string) {
+    return this.contactsService.findOne(request.user as User, +id);
   }
 
   @Patch(':id')
