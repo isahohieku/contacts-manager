aa5aa0a83d2e38155813d713aaa6aabb0b144ff5
Isah Ohieku <isahohieku@gmail.com>
2024-02-10 21:49:59 +0100

    Add phone-types seeder

---
+++ src/database/seeds/phone-types.seed.ts
@@ -0,0 +1,32 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+import { Connection } from 'typeorm';
+import { PhoneType } from 'src/phone-types/entities/phone-type.entity';
+import { PhoneTypeEnum } from 'src/phone-types/phone-type.enum';
+
+export default class CreatePhoneType implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<void> {
+    const count = await connection
+      .createQueryBuilder()
+      .select()
+      .from(PhoneType, 'PhoneType')
+      .getCount();
+
+    if (count === 0) {
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(PhoneType)
+        .values([
+          { id: PhoneTypeEnum.Mobile, name: 'Mobile' },
+          { id: PhoneTypeEnum.Home, name: 'Home' },
+          { id: PhoneTypeEnum.Work, name: 'Work' },
+          { id: PhoneTypeEnum.Fax, name: 'Fax' },
+          { id: PhoneTypeEnum['Toll-Free'], name: 'Toll-Free' },
+          { id: PhoneTypeEnum.VoIP, name: 'VoIP' },
+          { id: PhoneTypeEnum.Virtual, name: 'Virtual' },
+          { id: PhoneTypeEnum.Emergency, name: 'Emergency' },
+        ])
+        .execute();
+    }
+  }
+}
