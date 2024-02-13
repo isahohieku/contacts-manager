962ce02bd6c2614f268448ea14e5d12c270472a4
Isah Ohieku <isahohieku@gmail.com>
2024-02-13 20:37:22 +0100

    Improve Contact phone update

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -3,9 +3,10 @@ import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Contact } from './entities/contact.entity';
+import { Phone } from 'src/phones/entities/phone.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contact])],
+  imports: [TypeOrmModule.forFeature([Contact, Phone])],
   controllers: [ContactsController],
   providers: [ContactsService],
 })
