a827b161f9b51a05e6dfdfd909b0ad1886ae67f2
Isah Ohieku <isahohieku@gmail.com>
2024-02-15 17:24:38 +0100

    Improve email creation dto

--- src/emails/dto/create-email.dto.ts
+++ src/emails/dto/create-email.dto.ts
@@ -1,1 +1,18 @@
-export class CreateEmailDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional } from 'class-validator';
+import { Contact } from 'src/contacts/entities/contact.entity';
+import { EmailType } from 'src/email-types/entities/email-type.entity';
+
+export class CreateEmailDto {
+  @ApiProperty({ example: 'isahohieku@gmail.com' })
+  @IsOptional()
+  email: string | null;
+
+  @ApiProperty({ type: EmailType })
+  @IsOptional()
+  email_type?: EmailType;
+
+  @ApiProperty({ example: 1 })
+  @IsOptional()
+  contact: Contact;
+}
