36e6e2f4f0140d8cb822213e00849c300714c07a
Isah Ohieku <isahohieku@gmail.com>
2024-06-24 19:47:16 +0100

    Add countries seed

---
+++ src/database/seeds/countries.seed.ts
@@ -0,0 +1,24 @@
+import { Factory, Seeder } from 'typeorm-seeding';
+//TODO: Fix deprecation
+import { Connection } from 'typeorm';
+import { Country } from '../../countries/entities/country.entity';
+import { countryCodes } from '../../countries/countries';
+
+export default class CreateCountries implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<void> {
+    const count = await connection
+      .createQueryBuilder()
+      .select()
+      .from(Country, 'Country')
+      .getCount();
+
+    if (count === 0) {
+      await connection
+        .createQueryBuilder()
+        .insert()
+        .into(Country)
+        .values(countryCodes)
+        .execute();
+    }
+  }
+}
