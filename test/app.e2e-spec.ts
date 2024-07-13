44297afeb974c802468fec76aeffe221cfcf307e
Isah Ohieku <isahohieku@gmail.com>
2024-07-13 15:31:35 +0100

    Refactor app e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,7 +3,7 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('App Controller (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
@@ -15,10 +15,11 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should return 404 for GET /', () => {
+    return request(app.getHttpServer()).get('/').expect(404);
   });
 });
