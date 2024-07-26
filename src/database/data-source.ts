92697b3c4bede0308688b91d497dcde435a9871e
Isah Ohieku <isahohieku@gmail.com>
2024-07-26 23:08:25 +0100

    Refactor app datasource

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,4 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import config from '../../ormconfig.json';
 
-export const AppDataSource = new DataSource(config as DataSourceOptions);
+export const AppDataSource = new DataSource(
+  config as unknown as DataSourceOptions,
+);
