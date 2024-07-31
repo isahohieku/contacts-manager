c5c2cddb5bb50e9c8a2ab290454ffedbb4563126
Isah Ohieku <isahohieku@gmail.com>
2024-07-31 19:45:20 +0100

    Already implemented the task

--- src/emails/dto/create-email.dto.ts
+++ src/emails/dto/create-email.dto.ts
@@ -6,8 +6,6 @@ import { IsUniqueToContact } from '../../utils/validators/is-unique-to-contact.v
 import { ERROR_MESSAGES } from '../../utils/constants/generic/errors';
 import { Email } from '../entities/email.entity';
 
-// TODO: Pass entities directly into the contraint array rather than the name of the entity
-// Refactor the affected custom validator decorators
 export class CreateEmailDto {
   @ApiProperty({ example: 'isahohieku@gmail.com' })
   @IsEmail({}, { message: 'Email address is required' })
