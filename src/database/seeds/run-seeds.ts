28f963240d93ab9fce64649e4df899948457771f
Isah Ohieku <isahohieku@gmail.com>
2025-09-11 22:24:59 +0100

    Intentionally addin ts-ignore

--- src/database/seeds/run-seeds.ts
+++ src/database/seeds/run-seeds.ts
@@ -1,5 +1,6 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 
+// @ts-expect-error We want to use the ormconfig.json file
 import config from '../../../ormconfig.json';
 
 // Import all seed classes
